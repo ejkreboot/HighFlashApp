09f8612b8c8c7247b7108fd7cdf5f53a27e97c12
Eric Kort <eric@ericjkort.com>
2023-01-05 23:52:20 -0500

    logout fixes

--- src/routes/public/auth/logout/+page.server.js
+++
@@ -1,8 +0,0 @@
-
-import { redirect } from '@sveltejs/kit'
-
-export const load = async () => {
-  // we only use this endpoint for the api
-  // and don't need to see the page
-  throw redirect(302, '/public/auth/login')
-}
