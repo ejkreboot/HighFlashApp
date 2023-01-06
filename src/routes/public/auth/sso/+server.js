8310b8f90d2dadaaf4490a785c4eb2a274801f00
Eric Kort <eric@ericjkort.com>
2023-01-05 20:31:20 -0500

    addresses #11

--- src/routes/public/auth/sso/+server.js
+++ src/routes/public/auth/sso/+server.js
@@ -28,9 +28,7 @@ async function fetch_session(token) {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({"session_token": token})
   })
-  log("(HF 29) : Parsing response.")
   const body = await response.json();
-  log("(HF 33) : Response body: " + JSON.stringify(body))
   return {token: body.auth_token, destination: body.destination};
 }
 
@@ -43,26 +41,21 @@ export async function GET({ request, url }) {
   const idp_session_token = url.searchParams.get('session_token');
 
   if(idp_session_token) {
-    log("(HF 46) : Posting back to SSO for credentials for token " + idp_session_token)
     const session = await fetch_session(idp_session_token)
     try {
       const auth_token = jwt.verify(session.token, public_key, options);
       session_token = nanoid();
       destination = session.destination;
       const user = await User.upsert({ email: auth_token.email, token: session_token, status: "verified"})
-      console.log("USPSERTED USER: " + JSON.stringify(user))
     } catch(err) {
-        log("(HF 54) : No valid session found at SSO endpoint.")
         session_token = "";
         destination = BASEURL + "public/auth/login";
     }
   } else {
-    log("(HF 59) : No valid session token passed to /public/auth/sso")
     session_token = "";
     destination = BASEURL + "public/auth/login";
   }
 
-  log("(HF 64) : Responding with html redirect to " + destination)
   let res = new Response("<html><head><meta http-equiv='Refresh' content='0; url=" + destination + "'><head></html>");
   res.headers.append("Content-Type", "text/html; charset=utf-8");
   res.headers.append("Set-Cookie", "session=" + session_token + "; SameSite=Lax; HttpOnly; Path=/; Max-Age=86400");
---
+++ src/routes/public/util/log/+server.js
@@ -0,0 +1,29 @@
+import * as dotenv from 'dotenv' 
+dotenv.config()
+import { Logtail } from "@logtail/node";
+
+export async function POST({ request, params, url }) {
+    const log_token = process.env.HIGHFLASH_LOG_TOKEN || null;
+    let res;
+    if(log_token && log_token == url.searchParams.get("token")) {
+        const logtail = new Logtail(process.env.HIGHFLASH_LOGTAIL_TOKEN);
+        let msg = await request.json();
+        logtail.log(msg);
+        res = new Response(JSON.stringify({result: "OK"}));
+    } else {
+        const options = {
+            status:403,
+            headers: {
+              "Content-type" : "application/problem+json",
+              "Content-Language": "en"
+            }
+          }
+        const body = {
+            "title": "Authorization token missing",
+            "detail": "An authorization token must be provided in query string to use this service.",
+            "status": 403,
+        }
+        res = new Response(JSON.stringify(body), options);
+    }
+    return res;
+}
