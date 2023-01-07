bd5b222c3df56b1a66ea7efceeeb8d53c8621fc9
Eric Kort <eric@ericjkort.com>
2023-01-06 22:33:08 -0500

    logout endpoint

--- src/routes/public/auth/logout/+server.js
+++ src/routes/public/auth/logout/+server.js
@@ -2,7 +2,7 @@ export async function GET({ request, params }) {
     console.log("GET request at logout endpoint received.")
     const options = {
         headers: {
-            "Access-Control-Allow-Origin": "https://highflowpeds.com"
+            "Access-Control-Allow-Origin": "*"
         }
     }
     return new Response(JSON.stringify({result: "OK"}), options);
