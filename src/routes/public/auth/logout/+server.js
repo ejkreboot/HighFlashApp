40901b0e00143c370e7771eb440cc03fcab53420
Eric Kort <eric@ericjkort.com>
2023-01-10 23:13:11 -0500

    logout endpoint CORS

--- src/routes/public/auth/logout/+server.js
+++ src/routes/public/auth/logout/+server.js
@@ -2,18 +2,20 @@ import { User } from '$lib/server/user.js';
 
 export async function GET({ request, params, url }) {
     console.log("GET request at logout endpoint received with params:");
-    console.log(params)
     const email = url.searchParams.get('user_email');
+
     const headers = new Headers();
-    headers.append( "Access-Control-Allow-Origin", "https://highflowpeds.com");
+    headers.append( "Access-Control-Allow-Origin", "https://www.highflowpeds.com");
+    headers.append("Access-Control-Allow-Methods", "OPTIONS, POST, GET")
+
     const options = {
         headers: headers
     }
+    
     await User.update(
         { token: '' },
         { where: { email: email } }
     )
     const response = new Response(JSON.stringify({result: "OK"}), options);
-    console.log("RESPONSE (server.js line 17):" + JSON.stringify(response))
     return(response);
 }
