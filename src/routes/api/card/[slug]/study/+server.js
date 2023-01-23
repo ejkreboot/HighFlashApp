1f1a9ad9a445f4740e140e08944a4964585f7feb
Eric Kort <eric@ericjkort.com>
2023-01-23 12:07:36 -0500

    add card id to next card request on study endpoint to avoid repeated cards

--- src/routes/api/card/[slug]/study/+server.js
+++ src/routes/api/card/[slug]/study/+server.js
@@ -17,7 +17,7 @@ export async function POST({ request, params }) {
     });
   
   const scores = await c.study(user.email, card_id, data.score);
-  const nc = await c.next_card(user.email, card.category)
+  const nc = await c.next_card(user.email, card.category, card_id)
   await c.close_db()
   return new Response(JSON.stringify({score: scores, next_card: nc}));
 }
